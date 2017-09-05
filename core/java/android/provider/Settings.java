d9e960a358d7faec1258ec82418d56770e3380c1
maxwen <max.weninger@gmail.com>
2017-09-05 20:36:26 +0200

    base: add CPU info overlay
    Change-Id: I3c75a9c2837c9293822a33a95380cc1edb7f2b1a
    
    Signed-off-by: xyyx <xyyx@mail.ru>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -11561,6 +11561,11 @@ public final class Settings {
         public static final String SYSTEM_DEFAULT_ANIMATION = "system_default_animation";
 
         /**
+         * @hide
+         */
+        public static final String SHOW_CPU_OVERLAY = "show_cpu_overlay";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
