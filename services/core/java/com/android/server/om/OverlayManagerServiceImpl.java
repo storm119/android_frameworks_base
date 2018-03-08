1615c9329b41617893322eba3f4b9032ce71cc7b
storm119 <storm119@gmail.com>
2018-03-08 14:58:51 +0100

    fix: OMS: Add back overlay modified callback implementation  - after March update
    Signed-off-by: Robby Duran <storm119@gmail.com>

--- services/core/java/com/android/server/om/OverlayManagerServiceImpl.java
+++ services/core/java/com/android/server/om/OverlayManagerServiceImpl.java
@@ -288,8 +288,8 @@ final class OverlayManagerServiceImpl {
                 mPackageManager.getPackageInfo(overlayPackage.overlayTarget, userId);
 
         mSettings.init(packageName, userId, overlayPackage.overlayTarget,
-                overlayPackage.applicationInfo.getBaseCodePath(), overlayPackage.isStaticOverlay,
-                overlayPackage.overlayPriority);
+                overlayPackage.applicationInfo.getBaseCodePath(),
+                isPackageStaticOverlay(overlayPackage), overlayPackage.overlayPriority);
         try {
             if (updateState(targetPackage, overlayPackage, userId)) {
                 mListener.onOverlaysChanged(overlayPackage.overlayTarget, userId);
