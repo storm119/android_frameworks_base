f20c72f4f6924bddc1c8624ce88ba0af85fdff3e
Alex Yustasov <alex9yust@gmail.com>
2018-05-11 12:21:04 +0300

    ActivityManager: consider not AWAKE as sleep state
    mWakefulness may be ASLEEP, DREAMING and  DOZING for sleep
    
    Change-Id: I82e8172b1c0fcfc4c91a9c82aefc311e0999d1b2

--- services/core/java/com/android/server/am/ActivityManagerService.java
+++ services/core/java/com/android/server/am/ActivityManagerService.java
@@ -12505,7 +12505,7 @@ public class ActivityManagerService extends IActivityManager.Stub
     }
 
     boolean isSleepingLocked() {
-        return mSleeping;
+        return mSleeping && mWakefulness != PowerManagerInternal.WAKEFULNESS_AWAKE;
     }
 
     void onWakefulnessChanged(int wakefulness) {
