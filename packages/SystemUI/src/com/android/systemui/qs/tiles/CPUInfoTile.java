b44e781657a8e811b174a0b05bfb414705ade2b3
storm119 <storm119@gmail.com>
2018-07-03 01:37:34 +0200

    fix: derp

--- packages/SystemUI/src/com/android/systemui/qs/tiles/CPUInfoTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/CPUInfoTile.java
@@ -108,7 +108,7 @@ public class CPUInfoTile extends QSTileImpl<BooleanState> {
 
     @Override
     public int getMetricsCategory() {
-        return MetricsEvent.DIRTYTWEAKS;
+        return MetricsEvent.CUSTOM_QUICK_TILES;
     }
 
     @Override
--- packages/SystemUI/src/com/android/systemui/qs/tiles/ImmersiveTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/ImmersiveTile.java
@@ -122,6 +122,6 @@ public class ImmersiveTile extends QSTileImpl<BooleanState> {
 
     @Override
     public int getMetricsCategory() {
-        return MetricsEvent.DIRTYTWEAKS;
+        return MetricsEvent.CUSTOM_QUICK_TILES;
     }
 }
